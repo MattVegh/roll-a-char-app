5a0afea08df62717ba60261d7d7055d66e58e5a5
Matt Vegh <mdv240@gmail.com>
2019-11-21 14:43:37 -0600

    mod troubles

--- src/components/RollDisplay/RollDisplay.js
+++ src/components/RollDisplay/RollDisplay.js
@@ -6,65 +6,50 @@ export default class RollDisplay extends Component {
 //      modifier: ''
 //  }
 
- 
+// calculateModifier = () => {
+//     console.log('calc mod', this.props.currentRoll.stats)
+//     console.log('calc mod map', Object.keys(this.props.currentRoll.stats).map(stat => this.props.currentRoll.stats[stat]))
+//     Object.keys(this.props.currentRoll.stats).map(stat => {
+//         if (this.props.currentRoll.stats[stat] > 3 && this.props.currentRoll.stats[stat] < 6) {
+//             console.log('calc mod -3 for', this.props.currentRoll.stats[stat])
+//             return '-3'
+
+
+//         } else if (this.props.currentRoll.stats[stat] > 5 && this.props.currentRoll.stats[stat] < 8) {
+//             console.log('calc mod -2 for', this.props.currentRoll.stats[stat])
+//             return '-2'
+
+//         } else if (this.props.currentRoll.stats[stat] > 7 && this.props.currentRoll.stats[stat] < 10) {
+//             console.log('calc mod -1 for', this.props.currentRoll.stats[stat])
+//             return '-1'
+
+//         } else if (this.props.currentRoll.stats[stat] > 9 && this.props.currentRoll.stats[stat] < 12) {
+//             console.log('calc mod 0 for', this.props.currentRoll.stats[stat])
+//             return '0'
+
+//         } else if (this.props.currentRoll.stats[stat] > 11 && this.props.currentRoll.stats[stat] < 14) {
+//             console.log('calc mod +1 for', this.props.currentRoll.stats[stat])
+//             return '+1'
+
+//         } else if (this.props.currentRoll.stats[stat] > 13 && this.props.currentRoll.stats[stat] < 16) {
+//             console.log('calc mod +2 for', this.props.currentRoll.stats[stat])
+//             return '+2'
+
+//         } else if (this.props.currentRoll.stats[stat] > 15 && this.props.currentRoll.stats[stat] < 18) {
+//             console.log('calc mod +3 for', this.props.currentRoll.stats[stat])
+//             return '+3'
+
+//         } else if (this.props.currentRoll.stats[stat] > 17 && this.props.currentRoll.stats[stat] < 20) {
+//             console.log('calc mod +4 for', this.props.currentRoll.stats[stat])
+//             return '+4'
+
+//         }
+//     })
+// }
 
     render() {
 
-        // calculateModifier = () => {
-        //     console.log('calc mod', this.props.currentRoll.stats)
-        //     console.log('calc mod map', Object.keys(this.props.currentRoll.stats).map(stat => this.props.currentRoll.stats[stat]))
-        //     Object.keys(this.props.currentRoll.stats).map(stat => {
-        //         if (this.props.currentRoll.stats[stat] > 3 && this.props.currentRoll.stats[stat] < 6) {
-        //             console.log('calc mod -3 for', this.props.currentRoll.stats[stat])
-        //             this.setState({
-        //                 modifier: '-3'
-        //             }) 
         
-        //         } else if (this.props.currentRoll.stats[stat] > 5 && this.props.currentRoll.stats[stat] < 8) {
-        //             console.log('calc mod -2 for', this.props.currentRoll.stats[stat])
-        //             this.setState({
-        //                 modifier: '-2'
-        //             })
-        
-        //         } else if (this.props.currentRoll.stats[stat] > 7 && this.props.currentRoll.stats[stat] < 10) {
-        //             console.log('calc mod -1 for', this.props.currentRoll.stats[stat])
-        //             this.setState({
-        //                 modifier: '-1'
-        //             })
-        
-        //         } else if (this.props.currentRoll.stats[stat] > 9 && this.props.currentRoll.stats[stat] < 12) {
-        //             console.log('calc mod 0 for', this.props.currentRoll.stats[stat])
-        //             this.setState({
-        //                 modifier: '0'
-        //             })
-        
-        //         } else if (this.props.currentRoll.stats[stat] > 11 && this.props.currentRoll.stats[stat] < 14) {
-        //             console.log('calc mod +1 for', this.props.currentRoll.stats[stat])
-        //             this.setState({
-        //                 modifier: '+1'
-        //             })
-        
-        //         } else if (this.props.currentRoll.stats[stat] > 13 && this.props.currentRoll.stats[stat] < 16) {
-        //             console.log('calc mod +2 for', this.props.currentRoll.stats[stat])
-        //             this.setState({
-        //                 modifier: '+2'
-        //             })
-        
-        //         } else if (this.props.currentRoll.stats[stat] > 15 && this.props.currentRoll.stats[stat] < 18) {
-        //             console.log('calc mod +3 for', this.props.currentRoll.stats[stat])
-        //             this.setState({
-        //                 modifier: '+3'
-        //             })
-        
-        //         } else if (this.props.currentRoll.stats[stat] > 17 && this.props.currentRoll.stats[stat] < 20) {
-        //             console.log('calc mod +4 for', this.props.currentRoll.stats[stat])
-        //             this.setState({
-        //                 modifier: '+4'
-        //             })
-        
-        //         }
-        //     })
-        // }
         
         console.log('from RollDisplay', this.props.currentRoll)
 
@@ -95,7 +80,7 @@ export default class RollDisplay extends Component {
                             <td>{Object.values(this.props.currentRoll.originalRolls)[0]}</td>
                             <td></td>
                             <td>{this.props.currentRoll.stats.strength}</td>
-                            <td></td>
+                            {/* <td>{this.calculateModifier(this.props.currentRoll.stats.strength)}</td> */}
                         </tr>
                         <tr>
                             <td>Dex</td>
